7f202edc0108c618a0538377d73a127b2273bd5f
Micka <mickaelandriana06@gmail.com>
2026-05-26 17:51:07 +0300

    feat: implement ApplicationService with methods for applying and validating applications

---
+++ app/services/application.py
@@ -0,0 +1,38 @@
+from ..dependencies import SessionDep
+from ..repositories import CrudGen
+from ..models import Application
+from ..schemas import ApplicationIn
+from fastapi import status, HTTPException
+from sqlmodel import select
+
+
+class ApplicationService:
+    def __init__(self, session: SessionDep) -> None:
+        self._repository = CrudGen(Application, session)
+
+    async def applied_post(self, data: ApplicationIn) -> Application:
+        try:
+            new_data = Application(**data.model_dump())
+            return self._repository.add(new_data)
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+    async def validate_application(self, data: ApplicationIn) -> Application:
+        statement = select(Application).where(
+            Application.post_id == data.post_id,
+            Application.worker_id == Application.worker_id,
+        )
+        session = self._repository.session
+
+        application = session.exec(statement).first()
+
+        if not application:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Job Applied not found"
+            )
+
+        application.is_validated = True
+        session.add(application)
+        session.commit()
+        session.refresh(application)
+        return application
